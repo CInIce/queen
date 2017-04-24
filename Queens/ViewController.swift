8904d3ce1afd5242f42bdba66b45264245bf7b53
ltb <ltb@cin.ufpe.br>
2017-04-24 18:20:41 -0300

    Finished first version

--- Queens/ViewController.swift
+++ Queens/ViewController.swift
@@ -7,91 +7,93 @@
 //
 
 import UIKit
+import Charts
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var barChartView: BarChartView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-        
-        //        var child = [String](repeating:"",count:8);
-        //
-        //        let child2 = [1,2,2,4,5,2,7,8];
-        
-        //        var population = [[Int]](repeating: [Int] (repeating: 0,count:8), count:100)
-        
-        
-        
-        //        for row in 0..<boardElements.count{
-        //            var hits = [Int](repeating:0,count:8);
-        //            hits = countHits(child: boardElements[row],  hits:hits)
-        //            hits = countHitsDiagonal(child: boardElements[row],  hits:hits)
-        //
-        //        }
-        
-        //        var parents = selectParents(population: boardElements)
-        
-        //        var genotype = [[String]](repeating: [String] (repeating: "",count:8), count:100)
-        //        genotype = initPopulation(board: boardElements, genotype:genotype)
-        //
-        //
-        //        var board = [[Int]](repeating: [Int] (repeating: 0,count:8), count:8)
-        
-        //                print(str) // prints "10110"
-        //        var first = Individual(fenotype: [0,0,4,4,2,2,0,0])
-        
-        //        print(first.fitness)
-        //        var population:[Individual]  = [Individual(fenotype: [0,0,4,5,2,2,0,0]), Individual(fenotype: [1,2,0,4,5,2,7,6]), Individual(fenotype: [1,3,5,2,7,4,6,6])];
-        var population: [Individual] = initPopulation(size:1000)
-//        print(population, terminator:"\n\n")
-        //        population.sort { $0.fitness < $1.fitness }
-        //        print(population, terminator:"\n\n")
-        
+        var population: [Individual] = initPopulation(size:100)
         var x = 0
         var genX = 1000000000
         var bestFitness = 1000000000
+        var bestIndividual: Individual = Individual()
+        population.sort{ $0.fitness > $1.fitness }
         while(population[population.count-1].fitness > 0 && x < 1000){
+            population.shuffle()
+            population.shuffle()
+            population.shuffle()
             var parents = selectParents2(population: population)
-            //        print("These are parents: \(parents)", terminator:"\n\n")
             var childs = generateChilds(father1: parents[0], father2: parents[1])
-            //        print("These are chidls \(childs)", terminator: "\n\n")
-            
-            population.append(childs[0])
-            population.append(childs[1])
-            population.append(childs[2])
-            population.append(childs[3])
-            population.sort{ $0.fitness > $1.fitness }
-            population.remove(at: 0)
-            population.remove(at: 1)
-            population.remove(at: 2)
-            population.remove(at: 3)
-            if(bestFitness > population[population.count-1].fitness){
-                bestFitness = population[population.count-1].fitness
-                genX = x
+            if(childs.count != 2){
+                population.append(childs[0])
+                population.append(childs[1])
+                population.append(childs[2])
+                population.append(childs[3])
+                population.sort{ $0.fitness > $1.fitness }
+                population.remove(at: 0)
+                population.remove(at: 1)
+                population.remove(at: 2)
+                population.remove(at: 3)
+//                print("The best of gen \(x) is: \(population[population.count-1])", terminator: "\n\n")
+                if(bestFitness > population[population.count-1].fitness){
+                    bestIndividual = population[population.count-1]
+                    bestFitness = population[population.count-1].fitness
+                    genX = x
+                }
+                x += 1
             }
-            //            print(population)
-            population.shuffle()
-            population.shuffle()
-            population.shuffle()
-            x += 1
+            population.sort{ $0.fitness > $1.fitness }
         }
         
         
-        population.sort{ $0.fitness > $1.fitness }
-        print("This is the population: \(population)", terminator:"\n\n")
+        
+//        print("This is the population: \(population)", terminator:"\n\n")
+        print("\n\n")
         var (report, ocurrences) = countOcurrences(population: population)
         print("These are the configurations: \(report)")
         print("These are how many times: \(ocurrences)", terminator:"\n\n")
-
-        var (mean, variation) = meanAndStandardVariation(population: population)
-        print(mean)
-        print(variation)
         
-        print("This genX is:\(genX) and the best fitness is:\(bestFitness)")
+        var (mean, variation) = meanAndStandardVariation(population: population)
+        print("The mean is: \(mean)")
+        print("The variation is: \(variation)")
+        print("This genX is: \(genX)")
+        print("The best fitness is: \(bestFitness)")
+        print("Best individual is: \(bestIndividual)")
+        //        let formato:BarChartFormatter = BarChartFormatter()
+        //        let xaxis:XAxis = XAxis()
+        //        barChartView.xAxis.valueFormatter = IndexAxisValueFormatter(values: report)
+        
+        setChart(dataPoints: report, values: ocurrences)
     }
     
-    
+    func setChart(dataPoints:[Individual], values:[Int]){
+        //        let formato:BarChartFormatter = BarChartFormatter()
+        //        let xaxis:XAxis = XAxis()
+        
+        var dataEntries: [BarChartDataEntry] = []
+        var strings: [String] = []
+        for i in 0..<dataPoints.count {
+            //            formato.stringForValue(Double(i), axis: xaxis)
+            let dataEntry = BarChartDataEntry(x: Double(values[i]), y: Double(i))
+            dataEntries.append(dataEntry)
+            //            strings.append(dataPoints[i].genotype.joined(separator: "-"))
+            let myString = String(describing: index)
+            
+            strings.append(myString)
+            
+        }
+        barChartView.xAxis.valueFormatter = IndexAxisValueFormatter(values: strings)
+        //        xaxis.valueFormatter = formato
+        
+        let chartDataSet = BarChartDataSet(values: dataEntries, label: "Fitness")
+        
+        var chartData = BarChartData(dataSet: chartDataSet)
+        
+        barChartView.data = chartData
+    }
     
     func genPopulation(){
         
@@ -111,37 +113,17 @@ class ViewController: UIViewController {
         return population
     }
     
-    //    func selectParents(population:[[Int]])-> [[Int]]{
-    //        var parents = [[Int]](repeating: [Int] (repeating: 0,count:8), count:5)
-    //        var hits = [[Int]](repeating: [Int] (repeating: 0,count:8), count:5)
-    //
-    //        for index in 0..<5{
-    //            parents[index] = (population[Int(arc4random_uniform(100))])
-    //            hits[index] = countHits(child: parents[index],  hits:hits[index])
-    //            hits[index] = countHitsDiagonal(child: parents[index],  hits:hits[index])
-    //        }
-    //
-    //        var fitnessTable = Array<Int>()
-    //        for item in 0..<parents.count {
-    //            fitnessTable.append(calculateFitness(individual: hits[item]))
-    //        }
-    //        //        print(fitnessTable)
-    //
-    //
-    //        return parents
-    //    }
     
-    
-    func meanAndStandardVariation(population: [Individual]) -> (Int, Double){
-        var (mean, variation) = (0,0.0)
+    func meanAndStandardVariation(population: [Individual]) -> (Double, Double){
+        var (mean, variation) = (0.0,0.0)
         
         for child in population {
-            mean += child.fitness
+            mean += Double(child.fitness)
         }
-        mean /= population.count
+        mean /= Double(population.count)
         
         for child in population{
-            let fitness = child.fitness
+            let fitness = Double(child.fitness)
             let number = Decimal(fitness-mean)
             variation += (pow(Double(fitness-mean), 2))
         }
@@ -171,7 +153,7 @@ class ViewController: UIViewController {
     func selectParents2(population:[Individual])-> [Individual]{
         var parents: [Individual] = [Individual]()
         for index in 0..<5{
-            parents.append((population[Int(arc4random_uniform(100))]))
+            parents.append((population[Int(arc4random_uniform(UInt32(population.count)))]))
         }
         parents.sort { $0.fitness > $1.fitness }
         return [parents[3], parents[4]]
@@ -188,8 +170,7 @@ class ViewController: UIViewController {
     
     func board() -> [[Int]]{
         var board = [[Int]](repeating: [Int] (repeating: 0,count:8), count:8)
-        //        print("Tabuleiro:")
-        
+
         var number = 0
         for column in 0..<board.count{
             for row in 0..<board[0].count{
@@ -203,7 +184,6 @@ class ViewController: UIViewController {
     
     
     func countHits(child:[Int], hits:[Int]) -> [Int]{
-        //        var hits: Int = 0;
         var hits = hits
         for index1 in 0..<child.count{
             var secondIndex = (index1+1)%8
@@ -215,7 +195,6 @@ class ViewController: UIViewController {
                 secondIndex = (secondIndex+1)%8
                 x += 1
             }
-            //            print("Queen \(index1) got hits: \(hits)")
         }
         return hits
     }
@@ -227,15 +206,12 @@ class ViewController: UIViewController {
             var secondIndex = (index1+1)%8
             var x: Int = 0
             while(x < 8){
-                //                print("\(child[index1]), \(child[secondIndex]), \(abs(child[index1]-index1)), \(abs(child[secondIndex]-secondIndex))\n")
                 if(abs(child[index1]-child[secondIndex]) == abs(index1-secondIndex) && (index1 != secondIndex)){
                     hits[index1] += 1
                 }
                 secondIndex = (secondIndex+1)%8
                 x += 1
             }
-            //            print("Queen \(index1) got diagonal hits: \(hits)")
-            
         }
         return hits
     }
@@ -246,16 +222,10 @@ class ViewController: UIViewController {
         for index in 0..<child.count-1{
             _ = (child[index+1])
             _ = index+1
-            
-            
-            
             if( (child[index] - index) == (child[index+1]-(index+1))){
                 hits += 1
             }
-            
-            
         }
-        //        print(hits);
     }
     
     override func didReceiveMemoryWarning() {
@@ -267,6 +237,12 @@ class ViewController: UIViewController {
     func generateChilds(father1: Individual, father2: Individual) -> [Individual]{
         var childs: [Individual]
         var fenotypes: [[Int]] = [[Int]]()
+        
+        var mutationProb: Int = Int(arc4random_uniform(100))
+        
+        if(mutationProb > 90){
+            return [father1, father2, father1, father2];
+        }
         fenotypes.append([father1.fenotype[0],father1.fenotype[1],father1.fenotype[2]])
         fenotypes.append([father2.fenotype[0],father2.fenotype[1],father2.fenotype[2]])
         for index in 3..<father1.fenotype.count {
@@ -283,12 +259,12 @@ class ViewController: UIViewController {
         var genotypeS1 = [String]()
         var genotypeS2 = [String]()
         for index in 0..<8 {
-           genotype1.append(Array(first.genotype[index].characters))
+            genotype1.append(Array(first.genotype[index].characters))
             genotype2.append(Array(second.genotype[index].characters))
             genotypeS1.append(String(genotype1[index]))
             genotypeS2.append(String(genotype2[index]))
-            var mutationProb: Int = Int(arc4random_uniform(100))
-            if(mutationProb < 10){
+            mutationProb = Int(arc4random_uniform(100))
+            if(mutationProb < 40){
                 for genoIndex in 0...2{
                     
                     if (genotype1[index][genoIndex] == "0"){
@@ -302,7 +278,7 @@ class ViewController: UIViewController {
                 genotypeS1[index] = (String(genotype1[index]))
             }
             mutationProb = Int(arc4random_uniform(100))
-            if(mutationProb < 10){
+            if(mutationProb < 40){
                 for genoIndex in 0...2{
                     
                     if (genotype2[index][genoIndex] == "0"){
@@ -312,19 +288,24 @@ class ViewController: UIViewController {
                         genotype2[index][genoIndex] = "0"
                     }
                 }
+                mutated2 = true
+                genotypeS2[index] = (String(genotype2[index]))
             }
-            mutated2 = true
-            genotypeS2[index] = (String(genotype2[index]))
+            
         }
         
         childs = [Individual(fenotype: fenotypes[0]), Individual(fenotype: fenotypes[1])]
-//        if(mutated1){
-            childs.append(childs[0].translateToDecimal(genoType: genotypeS1))
-//        }
         
-//        if(mutated2){
-            childs.append(childs[1].translateToDecimal(genoType: genotypeS2))
-//        }
+        if(mutated1){
+            childs[0].byMutation = true
+        }
+        childs.append(childs[0].translateToDecimal(genoType: genotypeS1))
+        
+        
+        if(mutated2){
+            childs[0].byMutation = true
+        }
+        childs.append(childs[1].translateToDecimal(genoType: genotypeS2))
         childs.sort { $0.fitness < $1.fitness }
         return childs
         
@@ -340,9 +321,15 @@ class ViewController: UIViewController {
     struct Individual {
         var genotype: [String] = [String](repeatElement("", count: 8))
         var fenotype: [Int] = [Int](repeatElement(0, count: 8))
-        var fitness: Int = 0
+        var byMutation: Bool = false
+        var fitness: Int = 10000
         
-        
+        init(){
+            genotype = [String](repeatElement("", count: 8))
+            fenotype = [Int](repeatElement(0, count: 8))
+            fitness = 10000
+            byMutation = false
+        }
         
         init(fenotype:[Int]) {
             self.fenotype = fenotype
@@ -424,46 +411,6 @@ class ViewController: UIViewController {
             return hits
         }
     }
-    
-    
-    
-    
-    //
-    //        print(boardElements.count)
-    //        print(boardElements[0].count)
-    //            print("Row: \(boardElements[row])")
-    //            print("Hits: \(hits)")
-    //        for child in 0..<boardElements[0].count{
-    //            var hits = [Int](repeating:0,count:8);
-    //            print(boardElements[row][child])
-    //            }
-    //            hits = countHitsDiagonal(boardElements[child], hits)
-    //            hits = countHits(child: boardElements[child], hits:hits)
-    //        let num = 22
-    //        let str = String(num, radix: 2)
-    //        print(str)
-    //        var hits = [Int](repeating:0,count:8);
-    //        hits = countHits(child: child2,  hits:hits)
-    //        print(hits)
-    //        print("\n")
-    //        hits = countHitsDiagonal(child: child2, hits:hits)
-    //        print(hits)
-    //        print("\n")
-    
-    //    func initPopulation(board:[[Int]], genotype:[[String]]) -> [[String]]{
-    //        var genotype = genotype
-    //        for times in 0..<board.count{
-    //            for index in 0..<board[0].count{
-    //                //                let number: Int = Int(arc4random_uniform(8))
-    //                var str = String(board[times][index],radix:2)
-    //                str = pad(string: str, toSize: 3)
-    //                genotype[times][index] = str
-    //            }
-    //            //            print(genotype[times]);
-    //        }
-    //        return genotype
-    //    }
-    
 }
 
 extension MutableCollection where Indices.Iterator.Element == Index {
